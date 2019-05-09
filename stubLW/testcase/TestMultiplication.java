8ff771e7bb05aab9850fed38ac63422c6ba57bfe
tongbs <tongsteven79@gmail.com>
2019-05-09 16:22:57 +0800

    0413359

--- stubLW/testcase/TestMultiplication.java
+++ stubLW/testcase/TestMultiplication.java
@@ -73,37 +73,46 @@ public class TestMultiplication {
 
 		assertEquals(expectedANS, actualANS);
 	}
-/*
-	//@Test
+	@Test
 	public void testMultiplicationThreeNumberArrayList() {
 		// Test three number multiplications
+		int a = 3;
+		int b = 2;
+		int c = 6;
+		int expectedANS = 36;
 		// Here should init prepared answer
 
-		int expectedANS = 0;
 
 		// Here should new Factorization object with Mockito stub
 		// Here should ask stub object to act whatever we want
+		ArrayList<Integer> array = new ArrayList<Integer>(Arrays.asList(a, b, c));
 
+		Factorization factorizationStub = mock(Factorization.class);
+		when(factorizationStub.factorization(expectedANS)).thenReturn(array);
+		when(factorizationStub.factorizationRoot(anyInt(), anyBoolean())).thenReturn(-1);
 		Multiplication multiplication = new Multiplication();
 		int actualANS = multiplication.multiplicationWithFactorizationAbsolute(factorizationStub, expectedANS);
 
 		assertEquals(expectedANS, actualANS);
 	}
-
-	//@Test
+	@Test
 	public void testMultiplicationOneNumberArrayList() {
 		// Test one number multiplications, complete test coverage to 100%
+		int a = 9;
 		// Here should init prepared answer
 
-		int expectedANS = 0;
+		int expectedANS = -1;
 
 		// Here should new Factorization object with Mockito stub
 		// Here should ask stub object to act whatever we want
+		ArrayList<Integer> array = new ArrayList<Integer>(Arrays.asList(a));
 
+		Factorization factorizationStub = mock(Factorization.class);
+		when(factorizationStub.factorization(expectedANS)).thenReturn(array);
+		when(factorizationStub.factorizationRoot(anyInt(), anyBoolean())).thenReturn(-1);
 		Multiplication multiplication = new Multiplication();
 		int actualANS = multiplication.multiplicationWithFactorizationAbsolute(factorizationStub, expectedANS);
 
 		assertEquals(expectedANS, actualANS);
 	}
-*/
 }
